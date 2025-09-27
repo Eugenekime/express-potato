0902aaf9977353a602354d0b60eb021e555fa09f
Eugenekime <kimeugene0824@gmail.com>
2025-09-27 11:31:02 +0900

    fixed

--- routers/refreshTokens.router.js
+++ routers/refreshTokens.router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { refresh } from '../controllers/refreshTokens.controller';
+import { refresh } from '../controllers/refreshTokens.controller.js';
 
 const refreshRouter = Router();
 
