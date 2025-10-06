341df67e4c1d2379946d29b63a1e405c7102fda4
Eugenekime <kimeugene0824@gmail.com>
2025-10-06 12:08:29 +0900

    fixed deltet acc

--- controllers/deleteAcc.controller.js
+++ controllers/deleteAcc.controller.js
@@ -1,17 +1,25 @@
 import User from '../models/User.js';
 
 export const deleteAcc = async (req, res) => {
-  const { refreshToken } = req.body;
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      return res.status(400).json({ message: 'Нет токена' });
+    }
 
-  const user = await User.findOne({ refreshTokens: refreshToken });
+    const user = await User.findOne({ refreshTokens: { $in: [refreshToken] } });
+    if (!user) {
+      return res.status(404).json({ message: 'Пользователь не найден' });
+    }
 
-  if (!user)
-    return res.sendStatus(204).json({ message: 'Пользователь не найден' });
+    user.refreshTokens = user.refreshTokens.filter(
+      (token) => token !== refreshToken
+    );
+    await user.save();
 
-  user.refreshTokens = user.refreshTokens.filter(
-    (token) => token !== refreshToken
-  );
-  await user.save();
-
-  res.sendStatus(200).json({ message: 'Аккаунт удалён' });
+    return res.status(200).json({ message: 'Аккаунт удалён' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Ошибка сервера' });
+  }
 };
