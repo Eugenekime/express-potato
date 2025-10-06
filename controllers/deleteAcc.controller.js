e8a90e3d1e259e3d8d401aad73a8011926b13ace
Eugenekime <kimeugene0824@gmail.com>
2025-10-06 12:02:13 +0900

    fix delete router

--- controllers/deleteAcc.controller.js
+++ controllers/deleteAcc.controller.js
@@ -5,12 +5,13 @@ export const deleteAcc = async (req, res) => {
 
   const user = await User.findOne({ refreshTokens: refreshToken });
 
-  if (!user) return res.sendStatus(204);
+  if (!user)
+    return res.sendStatus(204).json({ message: 'Пользователь не найден' });
 
   user.refreshTokens = user.refreshTokens.filter(
     (token) => token !== refreshToken
   );
   await user.save();
 
-  res.sendStatus(200);
+  res.sendStatus(200).json({ message: 'Аккаунт удалён' });
 };
